c7ff420a24f79cbef47f4b9c766d45e1171c1b8b
prashanith007 <prashanith007@gmail.com>
2021-08-15 14:23:55 +0530

    Update Repos

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -32,6 +32,3 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
-
-
